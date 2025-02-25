0634e60e33da40a9e527aa6db850c0f492a39a8f
Andrii <yurkivandriy02@gmail.com>
2025-02-25 16:30:32 +0200

    Completed task lockdown-cafe

---
+++ app/cafe.py
@@ -0,0 +1,23 @@
+import datetime
+from typing import Any
+
+from app.errors import (
+    NotVaccinatedError,
+    OutdatedVaccineError,
+    NotWearingMaskError,
+)
+
+
+class Cafe:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def visit_cafe(self, visitor: dict) -> Any:
+        if "vaccine" not in visitor:
+            raise NotVaccinatedError("All friends should be vaccinated")
+        elif visitor["vaccine"]["expiration_date"] < datetime.date.today():
+            raise OutdatedVaccineError("All friends should be vaccinated")
+        elif visitor["wearing_a_mask"] is False:
+            raise NotWearingMaskError("Friends should buy masks")
+        else:
+            return f"Welcome to {self.name}"
